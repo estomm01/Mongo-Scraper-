eae386cbe18e738609e13da3917a57a0aac13b8e
Markus Maasikas <markus@hotmail.com>
2019-06-22 08:28:04 -0400

    updated server.js

--- server.js
+++ server.js
@@ -11,34 +11,20 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 4000;
 
-// Parse application/x-www-form-urlencoded
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.static("./public"));
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
 
 // connect to database
-mongoose.Promise = Promise;
-var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/foxsScrape";
-if(process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI)
-} else {
-    mongoose.connect(dbConnect);
-}
-// mongodb://foxsScrape:password12@ds119585.mlab.com:19585/heroku_hd8909ql;
-// Connect mongoose to our database
-/* mongoose.connect(dbConnect, function (error) {
-    // Log any errors connecting with mongoose
-    if (error) {
-        console.log(error);
-    }
-    // Or log a success message
-    else {
-        console.log("Mongoose connection is successful");
-    }
-}); */
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+
 var db = mongoose.connection;
 db.on('error',function(err){
     console.log('Mongoose Error',err);
