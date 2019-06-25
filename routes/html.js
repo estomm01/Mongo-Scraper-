5a27ea2e4a4c6152d793b66e53dc98ef8568d6fd
Markus Maasikas <markus@hotmail.com>
2019-06-25 12:20:29 -0400

    updated js and html

--- routes/html.js
+++ routes/html.js
@@ -1,15 +1,16 @@
 var path = require("path");
 
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "views/index.html"));
+     app.get("/", function (req, res) {
+         res.sendFile(path.join(__dirname, "/../views/index.html"));
+          //res.json({dummy: "test"});
     });
 
     app.get("/saved/all", function (req, res) {
-        res.sendFile(path.join(__dirname, "views/saved.html"));
+        res.sendFile(path.join(__dirname, "/../views/saved.html"));
     });
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "views/index.html"));
+        res.sendFile(path.join(__dirname, "/../views/index.html"));
     });
 }
